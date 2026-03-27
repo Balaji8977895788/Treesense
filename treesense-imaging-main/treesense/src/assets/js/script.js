6b73f2adfadc903b3be35baf474cddc9d05192d0
Balaji B <balajibandaru41@gmail.com>
2026-03-27 18:17:13 +0530

    inital
    initial

--- treesense-imaging-main/treesense/src/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript code to toggle mobile menu visibility
-const mobileMenuIcon = document.querySelector('.hamburger-icon');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-if (mobileMenuIcon && mobileMenu) {
-    mobileMenuIcon.addEventListener('click', () => {
-        mobileMenu.classList.toggle('active');
-    });
-}
